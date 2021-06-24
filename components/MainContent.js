3bec1a4633d2647b27c43929c4d33ad34a6d0b50
xanf-code <darshanaswath@gmail.com>
2021-06-24 20:22:06 +0530

    repo🙌

--- components/MainContent.js
+++ components/MainContent.js
@@ -4,7 +4,7 @@ function MainContent() {
             <main className="lg:pt-16">
                 <section>
                     <div className="items-center pt-4 lg:pt-8 md:flex-row px-6 lg:px-4 max-w-6xl mx-auto">
-                        hello
+                        <h1 className="text-gray-200 md:text-black">Hello</h1>
                     </div>
                 </section>
             </main>
