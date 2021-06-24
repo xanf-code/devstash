af9113c5e41ea6f4f06b6964d94c243d61d97cad
xanf-code <darshanaswath@gmail.com>
2021-06-25 00:31:47 +0530

    🎯🔥

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,26 +1,40 @@
 import Image from 'next/image'
 
 function MainContent() {
+    function onToggle() {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.classList.toggle("-translate-x-full")
+    }
     return (
-        <div className="transform: translateY(0px) translateZ(0px)">
-            <main >
+        <div>
+            <main className="flex">
+                {/* Sidebar */}
+                <div className="sidebar bg-gray-50 dark:bg-[#0A0806] w-[50%] lg:w-[24%] absolute inset-y-0 left-0 -translate-x-full transition duration-200 ease-in-out lg:fixed lg:translate-x-0">
+                    <h1 className="pt-24">
+                        sidebar
+                    </h1>
+                </div>
+                <div className="h-screen hidden max-w-xs w-full xl:block"></div>
                 {/* INTRO SECTION */}
-                <section>
-                    <div className="items-center pt-4 lg:pt-8 md:flex-row px-6 lg:px-4 mx-auto">
+                <section className="pt-14 min-h-screen">
+                    <div className="items-center pt-4 lg:pt-8 px-6 lg:px-4 mx-auto">
                         <div className="w-full mx-auto text-center lg:w-8/12">
-                            <h1 className="text-blue-600 text-lg font-bold font-montserrat uppercase tracking-widest">#devstash</h1>
+                            <button onClick={onToggle}>
+                                toggle
+                            </button>
+                            <h1 className=" text-blue-600 text-lg font-bold font-montserrat uppercase tracking-widest">#devstash</h1>
                             <h1 className=" tracking-tighter md:tracking-tight dark:text-white font-bold text-4xl md:text-5xl xl:text-6xl font-montserrat ">A list of
-                                resources for noobies and new devs <span>
-                                    <Image src="https://emojis.slackmojis.com/emojis/images/1580448086/7667/think-about-it.png?1580448086" alt="Picture of the author"
-                                        width={30}
-                                        height={30} />
+                                coding resources for noobies and new devs <span>
+                                    <Image src="https://emojis.slackmojis.com/emojis/images/1600706728/10521/meow_code.gif?1600706728" alt="Picture of the author"
+                                        width={40}
+                                        height={40} />
                                 </span></h1>
                             <h1 className=" text-gray-600 dark:text-gray-400 font-semibold text-base md:text-xl font-montserrat leading-relaxed">There are lots of tools to choose from, and it’s hard to find the right ones. That’s why I created this list – so you save time by easily finding the best stuff out there.</h1>
                         </div>
                     </div>
                 </section>
             </main>
-        </div>
+        </div >
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,13 @@ export default function Home() {
       </Head>
 
       <div
-        className={`selection:bg-purple selection:text-white ${active == true ? "dark" : ""
+        className={`min-h-screen selection:bg-purple selection:text-white ${active == true ? "dark" : ""
           }`}
       >
-        <div className="h-screen dark:bg-black bg-white">
+        <div className=" dark:bg-black bg-white">
           {/* APP HEADER */}
-          <header className="bg-[#fff] border-b-2 border-gray-100 dark:border-gray-900 dark:bg-black">
-            <div className="flex flex-row justify-between  items-center py-3 md:flex-row px-4 container max-w-5xl mx-auto">
+          <header className="bg-[#fff] border-b-2 border-gray-100 dark:border-gray-900 dark:bg-black z-50 fixed w-screen">
+            <div className="flex flex-row justify-between items-center py-3 md:flex-row px-4 container max-w-5xl mx-auto">
               <Link href="/">
                 <a>
                   <span className="font-open-sans font-black text-xl md:text-3xl drop-shadow-sm text-gradient bg-gradient-to-r from-[#780206] to-[#061161] dark:from-[#ddd6f3] dark:to-[#faaca8]">
@@ -40,7 +40,6 @@ export default function Home() {
           </header>
 
           {/* MAIN SECTION */}
-
           <MainContent />
 
           {/* App Footer */}
