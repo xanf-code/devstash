fc1b59e793ce5706dbba871a8f44cc081578e4dc
xanf-code <darshanaswath@gmail.com>
2021-06-26 10:32:39 +0530

    Chakra🎯

--- components/StashComponent.js
+++ components/StashComponent.js
@@ -1,8 +1,11 @@
+import { Button } from "@chakra-ui/react"
+
 function StashComponent() {
     return (
         <main>
             <section className="pt-20 min-h-screen lg:min-h-0 m-auto">
                 <h1 className=" text-blue-600 text-lg font-bold font-montserrat uppercase tracking-widest">Stash</h1>
+                <Button colorScheme="blue">Button</Button>
             </section>
         </main>
     )
